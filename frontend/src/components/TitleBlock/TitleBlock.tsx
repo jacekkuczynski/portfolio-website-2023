2291ba0e7ab3c4488ad8fa5ac69d9a6eaa0533c0
Jacek Kuczyński <idea.jacek@gmail.com>
2023-10-11 10:26:19 +0200

    Create TitleBlock.tsx

---
+++ frontend/src/components/TitleBlock/TitleBlock.tsx
@@ -0,0 +1,20 @@
+const TitleBlock = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="font-bold uppercase font-raleway text-titleSmall md:text-titleMedium lg:text-titleLarge text-whiteDimmed">
+        Enter{title}
+      </h1>
+      <h2 className="text-leadingSmall md:text-leadingMedium lg:text-leadingLarge text-grey3 leading-[1.4]">
+        {description}
+      </h2>
+    </div>
+  );
+};
+
+export default TitleBlock;
