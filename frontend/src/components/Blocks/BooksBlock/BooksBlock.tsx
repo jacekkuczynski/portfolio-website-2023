2bbd5fbe33903cbbb5cb9a7085b58b77857980b3
Jacek Kuczyński <idea.jacek@gmail.com>
2023-10-12 15:21:06 +0200

    wip block components

--- frontend/src/components/Blocks/BooksBlock/BooksBlock.tsx
+++ frontend/src/components/Blocks/BooksBlock/BooksBlock.tsx
@@ -5,7 +5,7 @@ const BooksBlock = (props: BookBlockT) => {
   const { books } = props;
   return (
     <div className="container w-full ">
-      <div className="flex flex-col items-center w-full h-full gap-2 my-8 md:gap-4 lg:gap-8">
+      <div className="flex flex-col items-center w-full h-full gap-2 md:gap-4 lg:gap-8">
         {books.map((book) => (
           <Book {...book} key={book.id} />
         ))}
